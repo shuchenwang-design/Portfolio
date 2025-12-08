1b39cefea9bd1df26044737f21d63bb183606ff4
Shuchen Wang <shuchenwangcc@gmail.com>
2025-12-08 06:15:23 +0000

    nav update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ tailwind.config = {
         extend: {
             colors: {
                 'rust-100': '#D35B39', 
+                'black-gradient': 'linear-gradient(90deg,rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%)' ,
                 'black': '#000000ff', // Example: You can tweak your global black here too
                 'white': '#ffffffff', // Example: An off-white for softer contrast
             },
